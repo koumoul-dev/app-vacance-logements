388cbec4d224cfe00e2b58b0abce648eecb0050f
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-08-14 09:34:42 +0200

    fix: city search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -89,8 +89,8 @@ export default {
       if (!this.search || this.search.length < 3) {
         this.cities = [...this.cities]
       } else {
-        const cities = (await axios.get('https://koumoul.com/data-fair/api/v1/datasets/base-officielle-des-codes-postaux/lines', { params: { q: this.search, q_mode: 'complete' } })).data.results
-        this.cities = cities.map(r => ({ text: r.nom_commune + ` (${r.code_postal})`, value: r.code_commune_insee }))
+        const cities = (await axios.get('https://datanova.laposte.fr/data-fair/api/v1/datasets/laposte-hexasmal/lines', { params: { q: this.search, q_mode: 'complete' } })).data.results
+        this.cities = cities.map(r => ({ text: r.nom_de_la_commune + ` (${r.code_postal})`, value: r.code_commune_insee }))
         if ('paris'.includes(this.search.toLowerCase())) this.cities.unshift({ text: 'PARIS', value: '75056' })
         if ('lyon'.includes(this.search.toLowerCase())) this.cities.unshift({ text: 'LYON', value: '69123' })
         if ('marseille'.includes(this.search.toLowerCase())) this.cities.unshift({ text: 'Marseille', value: '13055' })
